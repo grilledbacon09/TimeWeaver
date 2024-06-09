85e3480dbcf45f8bb92ee9fa90df4bb4e7da1c53
slee1148 <135320739+slee1148@users.noreply.github.com>
2024-06-09 20:46:40 +0900

    Update FixedEntity.kt

--- app/src/main/java/com/example/timeweaver/roomDB/FixedEntity.kt
+++ app/src/main/java/com/example/timeweaver/roomDB/FixedEntity.kt
@@ -6,11 +6,11 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "FixedEntity")
 class FixedEntity {
     @PrimaryKey(autoGenerate = true)
-    var id: String? = null,
-    var name: String = "",
-    var startH: Int,
-    var startM:Int,
-    var duration:Int,
-    var daylist: List<String> = listOf("Mon", "Tue", "Wed", "Thu", "Fri"),
-    var importance:Int
+    var id:Int? = null
+    var name: String?=""
+    var startH:Int? = 0
+    var startM:Int? = 0
+    var duration:Int? = 0
+    var daylist: List<String> = listOf("Mon", "Tue", "Wed", "Thu", "Fri")
+    var importance:Int = 0
 }
