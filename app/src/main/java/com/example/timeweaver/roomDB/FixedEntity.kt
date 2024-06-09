b8411bebe8e12dce62386a370c0af5d67b251622
slee1148 <135320739+slee1148@users.noreply.github.com>
2024-06-09 18:38:01 +0900

    Update FixedEntity.kt

--- app/src/main/java/com/example/timeweaver/roomDB/FixedEntity.kt
+++ app/src/main/java/com/example/timeweaver/roomDB/FixedEntity.kt
@@ -1,7 +1,9 @@
 package com.example.timeweaver.roomDB
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "FixedEntity")
 class FixedEntity {
     @PrimaryKey(autoGenerate = true)
     var id:Int? = null
