f31a6759d8f2e40372a4e5ada7179c162a826e94
slee1148 <135320739+slee1148@users.noreply.github.com>
2024-06-12 18:17:54 +0900

    Update FixedRepository.kt

--- app/src/main/java/com/example/timeweaver/roomDB/FixedRepository.kt
+++ app/src/main/java/com/example/timeweaver/roomDB/FixedRepository.kt
@@ -4,17 +4,22 @@ import androidx.lifecycle.LiveData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import androidx.lifecycle.map
 
-class FixedRepository(db:FixedDatabase) {
+class FixedRepository(db: FixedDatabase) {
     val FixedDAO = db.fixedDao()
 
-    fun insert(fixed: FixedEntity){
+    suspend fun insert(fixed: FixedEntity){
         FixedDAO.insert(fixed)
     }
 
 
     fun getAll(): LiveData<List<FixedEntity>> {
-        return FixedDAO.getAll()
+        return FixedDAO.getAll().map { fixedEntities ->
+            fixedEntities.map { entity ->
+                FixedEntity(entity.name, entity.startH, entity.days, entity.duration)
+            }
+        }
     }
 
     fun delete(fixed: FixedEntity){
