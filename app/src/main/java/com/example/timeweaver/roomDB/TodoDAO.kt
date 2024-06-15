2faabd711cd172f5ac7aecbf13065c7662b98242
slee1148 <135320739+slee1148@users.noreply.github.com>
2024-06-15 17:02:59 +0900

    Update TodoDAO.kt

--- app/src/main/java/com/example/timeweaver/roomDB/TodoDAO.kt
+++ app/src/main/java/com/example/timeweaver/roomDB/TodoDAO.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import java.nio.charset.CodingErrorAction.REPLACE
 
 @Dao
@@ -12,9 +13,9 @@ interface TodoDAO {
     @Insert
     fun insert(todo: TodoEntity)
 
-    //(onConflict = REPLACE)
-    /*@Query("SELECT * FROM todo WHERE date = :date")
-    fun getAllByDate(date: String): LiveData<List<TodoEntity>?>*/
+    @Update
+    suspend fun update(todo: TodoEntity)
+
 
     @Delete
     fun delete(todo: TodoEntity)
