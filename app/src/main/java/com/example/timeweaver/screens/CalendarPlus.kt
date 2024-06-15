4103a94fbf07352bbce400fdfe70dd0b61b84958
slee1148 <135320739+slee1148@users.noreply.github.com>
2024-06-15 11:25:38 +0900

    Update CalendarPlus.kt

--- app/src/main/java/com/example/timeweaver/screens/CalendarPlus.kt
+++ app/src/main/java/com/example/timeweaver/screens/CalendarPlus.kt
@@ -137,9 +137,9 @@ fun CalendarPlus(navController: NavHostController,month: String,day:String,date:
                 onClick = {
                     if(scheduleName!=""&&estimatedTimeH!=null){
                         val task = Task(scheduleName, navViewModel.tasklist.size, importance1, false, once,deadline,timeH)
+                        val todo = TodoEntity(scheduleName, navViewModel.tasklist.size, importance1, false, once, deadline, timeH)
                         navViewModel.tasklist.add(task)
                         // Task 정보를 로그에 출력
-                        val todo = TodoEntity(scheduleName, estimatedTimeH.toInt(), once, importance.toInt(), id)
                         GlobalScope.launch(Dispatchers.IO) {
                             todoRepository.insert(todo)
                         }
